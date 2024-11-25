eb470b8ffa56ef56d548402f2845ba2f134b918e
Richard Neureuther <richard.neureuther@uni-konstanz.de>
2024-11-25 17:35:31 +0100

    progress a03

--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -27,16 +27,21 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     #this is the most important feature of this file. We can collect all the variables used on the html pages here
-
-    #Welcome
-    screen_height = models.IntegerField(initial=-999)
-    screen_width = models.IntegerField(initial=-999)
 #The Variables are structured on the base of pages
     name_question = models.StringField(label="Please enter your name here:")
+
     age_question = models.IntegerField(label="Please enter your age here:", max= 120, min=1)
+    
     study_field_question = models.StringField(label="Please enter your field of study here:")
+
     satisfaction = models.IntegerField(initial=-999, label="Please select the value that is most fitting:")
+
     additional_comments = models.StringField(label="Is there anything else you want to add?")
+
     windows_interface = models.IntegerField(initial=-999, label="Please select the value that is most fitting:")
+
     iOS_interface = models.IntegerField(initial=-999, label="Please select the value that is most fitting:")
+
+    screen_height = models.IntegerField(initial=-999)
+    screen_width = models.IntegerField(initial=-999)
                               
