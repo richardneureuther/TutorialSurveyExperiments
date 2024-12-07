d92f6c00e53eaa0ea20bbd81cd993da725bcc204
Richard Neureuther <richard.neureuther@uni-konstanz.de>
2024-12-07 13:48:01 +0100

    Completed A05

--- otree-example/bot-testing/bot_testing.py
+++ otree-example/bot-testing/bot_testing.py
@@ -12,13 +12,22 @@ def build_driver():
     # Set up the driver
     return webdriver.Chrome() 
 
-def check_exists_by_xpath(driver, xpath):
+#detect if the all sessions are full 
+def detect_session_full(driver):
     try:
-        x = driver.find_element(By.XPATH, xpath)
-        if x.is_displayed():
-            return 1
+        driver.find_element(By.XPATH,  '//*[@id="form"]/div/button')
+        return False
     except NoSuchElementException:
-        return 0
+        return True
+    
+#detect if the bot is currently on a screenout page 
+def detect_screenout(driver):
+    try:
+        #Try to locate an element that exists on DemoPage_1 return false if found True  if not found 
+        driver.find_element(By.XPATH, '//*[@id="id_study_field_question"]')
+        return False  
+    except NoSuchElementException:
+        return True
 
 #handle welcome page button 
 def welcome_page(driver):
@@ -32,7 +41,7 @@ def welcome_page(driver):
 def demopage_0(driver,run_number):
 #fill out age question (random 1 to 40)
     xpath_age  = '//*[@id="id_age_question"]'
-    age = random.randint(1,40)
+    age = random.randint(1,65)
     driver.find_element(By.XPATH, xpath_age).send_keys(str(age))
      
      #randomly select a gender 
@@ -91,21 +100,22 @@ def demopage_4(driver):
     #implement a scroller so the radio question can be proberly accessed on the page 
     def scroller(driver, element):
         driver.execute_script("arguments[0].scrollIntoView(true);", element)
-        time.sleep(0.3)  #short scroll timer 
+        #short scroll timer 
+        time.sleep(0.3)  
         element.click()
 
-    #extract src attribute to handle the detect the images 
+    #extract src attribute to detect the images 
     image_element = driver.find_element(By.XPATH, '//img')
     image_src = image_element.get_attribute('src')
     
     #check which image is displayed and assign the correct name 
     if 'windows_phone.png' in image_src:
-        print("Windows Phoneis shown.")
+        print(" Windows Phone is shown.")
         name = 'windows_interface'
 
     elif 'IOS_10_phone.png' in image_src:
-        print("iOS Phone is shown.")
-        name = ' iOS_interface'
+        print(" iOS Phone is shown.")
+        name = 'iOS_interface'
     
     #randomly select one button in the radio question 
     interface_options = driver.find_elements(By.NAME, name)
@@ -119,11 +129,14 @@ def demopage_4(driver):
 
 #handle popout questions
 def popout(driver):
+
     yes = '//*[@id="UniYes"]'
     no = '//*[@id="UniNo"]'
     select = random.randint(0, 1)
+
+    #randomly generate a text input
     input_text = ''.join(random.choice(string.ascii_letters) for i in range(random.randint(1, 10)))
-    
+    #check which option was selected and send input 
     if select == 0:
         driver.find_element(By.XPATH, yes).click()
         driver.find_element(By.XPATH, '//*[@id="divYes"]/input').send_keys(input_text)
@@ -145,33 +158,48 @@ def end_page(driver):
 
 #method to run the bots n times over the survey 
 def run_bots(runs,link):
-    driver = build_driver()  # initialize the driver
+    #initialize driver and start the loop
+    driver = build_driver()
     for i in range(runs):
         driver.get(link)
-        if check_exists_by_xpath(driver, '//*[@id="form"]/div/button') == 1:
-            x = welcome_page(driver) # check whether they are eligible
-            if x == 1:  # then they are not eligible, otherwise no next page
+
+        try:
+            #if the session is full abort the loop
+            if detect_session_full(driver):
+                print(f"Bot_{i} encountered session full screen. Stopping...")
+                break  
+
+            #fill out the first 2 pages 
+            welcome_page(driver)
+            demopage_0(driver, i)
+            
+            #check wether the bot is currently encountering a screenoutdue to quotas (age/gender), and if so skip to the next bot 
+            if detect_screenout(driver):
+                print(f"Bot_{i} redirected to screenout or quota page. Skipping...")
                 continue
+            
+            #continue with the pages
+            demopage_1(driver)
+            demopage_2(driver)
+            demopage_3(driver)
+            demopage_4(driver)
+            popout(driver)
+            end_page(driver)
+            print(f"Bot_{i} passed successfully.")
+    
+        #catch error, print it in the console and continue with next bot 
+        except Exception as e:
+            print(f"Bot_{i} encountered an error: {e}")
 
-        #order of pages shown
-        welcome_page(driver)
-        demopage_0(driver,i)
-        demopage_1(driver)
-        demopage_2(driver)
-        demopage_3(driver)
-        demopage_4(driver)
-        popout(driver)
-        end_page(driver)
-        
-        #print the current bot run 
-        print(f'Bot_{i} passed')
-
-    #after completion, print success in the console
-    print("Success!")
-
+    #after completion print Assignment 5 checklist in the console 
+   
+    print( "\nChecklist Assignment 4:\n"
+        "- Automate each survey step\n"
+        "- Randomly select answers/inputs + handle errors\n"
+        "- Ran bot multiple times, worked each time")
 
 #link to the current session being tested 
-link = 'http://localhost:8000/join/tanabusu'
+link = 'http://localhost:8000/join/darijapu'
 
-#run the bots
-run_bots(runs=20, link=link)
+#run the bots (test with more than 20 iterations to see if the screen out detectors work correctly )
+run_bots(runs=21, link=link)
--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -26,7 +26,7 @@ class Subsession(BaseSubsession):
      def creating_session(self):
         if 'gender_quotas' not in self.session.vars:
              #this for quota you wish to fill:
-            self.session.vars['gender_quotas'] = {1: 5, 2: 1, 3: 2, 4: 5}
+            self.session.vars['gender_quotas'] = {1: 5, 2: 5, 3: 5, 4: 5}
         # Create gender group counts
         # this will also be displayed in the data you download
         for gender in self.session.vars['gender_quotas'].keys():
